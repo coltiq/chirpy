8334ea4ca63bf9628a1300928983d70475c9328b
coltiq <coltonherren97@gmail.com>
2024-06-03 07:37:52 -0500

    metrics

--- main.go
+++ main.go
@@ -1,24 +1,61 @@
 package main
 
 import (
+    "fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+    fileserverHits  int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+        cfg.fileserverHits++
+        next.ServeHTTP(w, r)
+    })
+}
+
 func initServer() {
     const filepathRoot = "."
-    const port = "8080"
-
-    mux := http.NewServeMux()
-    mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
-
-    srv := &http.Server{
-                Addr: ":" + port,
-                Handler: mux,
+    const port = ":8080"
+    apiCfg := &apiConfig{
+            fileserverHits: 0,
     }
 
-    log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
-    log.Fatal(srv.ListenAndServe())
+    mux := http.NewServeMux()
+
+    // Register a custom Handler
+    http.HandleFunc("/healthz", healthHandler)
+
+    fileServer := http.FileServer(http.Dir(filepathRoot))
+
+    // Metrics Handler
+    mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+    mux.HandleFunc("/metrics", apiCfg.hitsHandler)
+    mux.HandleFunc("/reset", apiCfg.resetHandler)
+
+    log.Fatal(http.ListenAndServe(port, mux))
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) hitsHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+    cfg.fileserverHits = 0
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits)))
 }
 
 func main() {
