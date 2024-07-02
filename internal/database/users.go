4bd440c63a36be773dfe766880c9e259fbb60547
coltiq <dev@coltiq.com>
2024-07-02 15:37:17 -0500

    Update Users in DB

--- internal/database/users.go
+++ internal/database/users.go
@@ -37,6 +37,29 @@ func (db *DB) CreateUser(email, hashedPassword string) (User, error) {
 	return newUser, nil
 }
 
+func (db *DB) UpdateUser(id int, email, hashedPassword string) (User, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return User{}, err
+	}
+
+	user, ok := dbStructure.Users[id]
+	if !ok {
+		return User{}, ErrNotExist
+	}
+
+	user.Email = email
+	user.HashedPassword = hashedPassword
+	dbStructure.Users[id] = user
+
+	err = db.writeDB(dbStructure)
+	if err != nil {
+		return User{}, err
+	}
+
+	return user, nil
+}
+
 func (db *DB) GetUser(id int) (User, error) {
 	dbStructure, err := db.loadDB()
 	if err != nil {
